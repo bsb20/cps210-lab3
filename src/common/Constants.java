407c8987f7a6bd3e9a12dbedc1d0c879ab88e187
Jim Posen <jim.posen@gmail.com>
2012-12-01 11:57:32 -0500

    Wrote DFid block() and offset() methods.

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
 	public static final int BLOCK_SIZE = 1024; // 1kB
 	public static final int NUM_OF_INODES = 512; //max of 512 files 
 	public static final int CACHE_SIZE = 1024; //guess from Ian 
+    public static final int INODE_SIZE = 256;
 	/* DStore Operation types */
 	public enum DiskOperationType {
 		READ, WRITE
--- src/common/DFileID.java
+++ src/common/DFileID.java
@@ -1,9 +1,47 @@
 package common;
 
 /* typedef DFileID to int */
-public class DFileID {
+public class DFileID
+{
 	private int _dFID;
-	DFileID(int dFID) {
+
+
+	public DFileID(int dFID) {
 		_dFID = dFID;
 	}
+
+    public int block()
+    {
+        if (_dFID == 0) {
+            // First block may contain disk metadata
+            return 0;
+        }
+        else if (_dFID <= NUM_OF_INODES) {
+            // Multiple inodes share a block starting at block 1
+            return (_dFID - 1) * INODE_SIZE / BLOCK_SIZE + 1;
+        }
+        else {
+            // Regular file blocks take up full block
+            return 1 + // First block is metadata
+                (Constants.NUM_OF_INODES * Constants.INODE_SIZE /
+                 Constants.BLOCK_SIZE) +  // Number of inode blocks
+                (_dFID - NUM_OF_INODES - 1);
+        }
+    }
+
+    public int offset()
+    {
+        if (_dFID == 0) {
+            // First block may contain disk metadata
+            return 0;
+        }
+        else if (_dFID <= NUM_OF_INODES) {
+            // Multiple inodes share a block starting at block 1
+            return (_dFID - 1) * INODE_SIZE % BLOCK_SIZE;
+        }
+        else {
+            // Regular file blocks take up full block
+            return 0;
+        }
+    }
 }
