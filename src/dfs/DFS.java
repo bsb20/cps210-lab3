f64a64b5aef7d122a787e11f120b96731e3fe1a6
Jim Posen <jim.posen@gmail.com>
2012-12-01 14:07:11 -0500

    Changed DFS methods to match given interface with DFileIDs.

--- src/dfs/DFS.java
+++ src/dfs/DFS.java
@@ -64,6 +64,150 @@ public class DFS {
         return dFiles;
     }
 
+	/*
+	 * creates a new DFile and returns the DFileID, which is useful to uniquely
+	 * identify the DFile
+	 */
+	public synchronized int createDFile() {
+        int dFID = myFreeINodes.first();
+        myFreeINodes.remove(dFID);
+		DFileID newFile = new DFileID(dFID);
+		myDFiles.put(newFile, new LockState(true,1));
+		byte[] buffer = new byte[Constants.BLOCK_SIZE];
+		DBuffer container = myDBCache.getBlock(newFile.block());
+		container.read(buffer, 0, Constants.BLOCK_SIZE);
+		container.release();
+		ByteBuffer b = ByteBuffer.allocate(Constants.INODE_SIZE);
+		int nextBlock = myFreeBlocks.first();
+		myFreeBlocks.remove(0);
+		b.putInt(1);
+		b.putInt(nextBlock);
+		for (int i = 0; i < Constants.INODE_SIZE - 8; i += 4) {
+			b.putInt(0);
+		}
+		byte[] newINode = b.array();
+		for (int j = 0; j < Constants.INODE_SIZE; j++) {
+			buffer[j + newFile.offset()] = newINode[j];
+		}
+		container = myDBCache.getBlock(newFile.block());
+		container.write(buffer, 0, Constants.BLOCK_SIZE);
+		container.release();
+		myDFiles.get(newFile).numReaders--;
+		return newFile.id();
+	}
+
+	/* destroys the file specified by the DFileID */
+	public void destroyDFile(DFileID dFID) {
+		ArrayList<Integer> iNodeInfo = parseINode(dFID);
+
+		formatBlock(dFID.id());
+		myFreeINodes.remove(dFID);
+        acquireWriteLock(dFID);
+		myDFiles.remove(dFID);
+        releaseWriteLock(dFID);
+		for (int i = 1; i < iNodeInfo.size(); i++) {
+			myFreeBlocks.remove(iNodeInfo.get(i));
+			formatBlock(iNodeInfo.get(i));
+		}
+	}
+
+	/*
+	 * reads the file dfile named by DFileID into the buffer starting from the
+	 * buffer offset startOffset; at most count bytes are transferred
+	 */
+	public int read(DFileID dFID, byte[] buffer, int startOffset, int count) {
+		ArrayList<Integer> iNodeInfo = parseINode(dFID);
+		int bytesRead = 0;
+
+		for (int i = 1; i <= iNodeInfo.get(0); i++) {
+			DBuffer toRead = myDBCache.getBlock(iNodeInfo.get(i));
+			bytesRead += toRead.read(buffer, startOffset + bytesRead, count
+					- bytesRead);
+			toRead.release();
+			if (bytesRead >= count)
+				break;
+		}
+		return bytesRead;
+	}
+
+	/*
+	 * writes to the file specified by DFileID from the buffer starting from the
+	 * buffer offset startOffsetl at most count bytes are transferred
+	 */
+	public int write(DFileID dFID, byte[] buffer, int startOffset, int count) {
+		if (myFreeBlocks.size() == 0) {
+			System.out.println("VOLUME SIZE EXCEEDED");
+			return -1;
+		}
+		ArrayList<Integer> iNodeInfo = parseINode(dFID);
+		int bytesWritten = 0;
+		int vBlockNumber = 1;
+		while (bytesWritten < count
+				&& buffer.length > (startOffset + bytesWritten)) {
+			DBuffer nextBlock;
+			if (vBlockNumber < iNodeInfo.get(0)) {
+				nextBlock = myDBCache.getBlock(iNodeInfo.get(vBlockNumber));
+				vBlockNumber++;
+			} else {
+				DBuffer iNodeBlock = myDBCache.getBlock(dFID.block());
+				byte[] blockBuffer = new byte[Constants.BLOCK_SIZE];
+				iNodeBlock.read(buffer, 0, Constants.BLOCK_SIZE);
+				ByteBuffer b = ByteBuffer.allocate(Constants.INODE_SIZE);
+				int nextPBlockNumber;
+				synchronized (this) {
+					nextPBlockNumber = myFreeBlocks.first();
+					myFreeBlocks.remove(0);
+				}
+				iNodeInfo.set(0, iNodeInfo.get(0) + 1);
+				iNodeInfo.add(nextPBlockNumber);
+				try {
+					for (int i : iNodeInfo)
+						b.putInt(i);
+
+				} catch (BufferOverflowException e) {
+					System.out.println("MAX FILE SIZE EXCEEDED");
+					return -1;
+				}
+				for (int j = 0; j < Constants.INODE_SIZE; j++) {
+					blockBuffer[j + dFID.offset()] = b.array()[j];
+				}
+				iNodeBlock.write(blockBuffer, 0, Constants.BLOCK_SIZE);
+				iNodeBlock.release();
+				nextBlock = myDBCache.getBlock(nextPBlockNumber);
+				vBlockNumber++;
+			}
+			bytesWritten += nextBlock.write(buffer, startOffset + bytesWritten,
+					count - bytesWritten);
+			nextBlock.release();
+		}
+		return count;
+	}
+
+	/* returns the size in bytes of the file indicated by DFileID. */
+	public int sizeDFile(DFileID dFID) {
+		ArrayList<Integer> iNodeInfo = parseINode(dFID);
+		return iNodeInfo.get(0)*Constants.BLOCK_SIZE;
+	}
+
+	/*
+	 * List all the existing DFileIDs in the volume
+	 */
+	public void sync() {
+		myDBCache.sync();
+	}
+
+	/* Zeros out a block */
+	private void formatBlock(int dFID) {
+		byte[] zeroBuffer = new byte[Constants.BLOCK_SIZE];
+		for (byte b : zeroBuffer) {
+			zeroBuffer[b] = 0;
+		}
+		DBuffer iNodeToKill = myDBCache.getBlock(dFID);
+		iNodeToKill.write(zeroBuffer, 0, Constants.BLOCK_SIZE);
+		iNodeToKill.release();
+	}
+
+
 	// scan VDF to form the metadata stuctures
 	private void initializeMData() {
 		formatFreeBlockList(); // set free block list to all free blocks
@@ -104,151 +248,7 @@ public class DFS {
 		return parsedINode;
 	}
 
-	/*
-	 * creates a new DFile and returns the DFileID, which is useful to uniquely
-	 * identify the DFile
-	 */
-	public synchronized int createDFile() {
-        int dFID = myFreeINodes.first();
-        myFreeINodes.remove(dFID);
-		DFileID newFile = new DFileID(dFID);
-		myDFiles.put(newFile, new LockState(true,1));
-		byte[] buffer = new byte[Constants.BLOCK_SIZE];
-		DBuffer container = myDBCache.getBlock(newFile.block());
-		container.read(buffer, 0, Constants.BLOCK_SIZE);
-		container.release();
-		ByteBuffer b = ByteBuffer.allocate(Constants.INODE_SIZE);
-		int nextBlock = myFreeBlocks.first();
-		myFreeBlocks.remove(0);
-		b.putInt(1);
-		b.putInt(nextBlock);
-		for (int i = 0; i < Constants.INODE_SIZE - 8; i += 4) {
-			b.putInt(0);
-		}
-		byte[] newINode = b.array();
-		for (int j = 0; j < Constants.INODE_SIZE; j++) {
-			buffer[j + newFile.offset()] = newINode[j];
-		}
-		container = myDBCache.getBlock(newFile.block());
-		container.write(buffer, 0, Constants.BLOCK_SIZE);
-		container.release();
-		myDFiles.get(newFile).numReaders--;
-		return newFile.id();
-	}
-
-	/* destroys the file specified by the DFileID */
-	public void destroyDFile(int dFID) {
-		ArrayList<Integer> iNodeInfo = parseINode(new DFileID(dFID));
-
-		formatBlock(dFID);
-		myFreeINodes.remove(dFID);
-        acquireWriteLock(dFID);
-		myDFiles.remove(dFID);
-        releaseWriteLock(dFID);
-		for (int i = 1; i < iNodeInfo.size(); i++) {
-			myFreeBlocks.remove(iNodeInfo.get(i));
-			formatBlock(iNodeInfo.get(i));
-		}
-	}
-
-	/* Zeros out a block */
-	private void formatBlock(int dFID) {
-		byte[] zeroBuffer = new byte[Constants.BLOCK_SIZE];
-		for (byte b : zeroBuffer) {
-			zeroBuffer[b] = 0;
-		}
-		DBuffer iNodeToKill = myDBCache.getBlock(dFID);
-		iNodeToKill.write(zeroBuffer, 0, Constants.BLOCK_SIZE);
-		iNodeToKill.release();
-	}
-
-	/*
-	 * reads the file dfile named by DFileID into the buffer starting from the
-	 * buffer offset startOffset; at most count bytes are transferred
-	 */
-	public int read(int dFID, byte[] buffer, int startOffset, int count) {
-		ArrayList<Integer> iNodeInfo = parseINode(new DFileID(dFID));
-		int bytesRead = 0;
-
-		for (int i = 1; i <= iNodeInfo.get(0); i++) {
-			DBuffer toRead = myDBCache.getBlock(iNodeInfo.get(i));
-			bytesRead += toRead.read(buffer, startOffset + bytesRead, count
-					- bytesRead);
-			toRead.release();
-			if (bytesRead >= count)
-				break;
-		}
-		return bytesRead;
-	}
-
-	/*
-	 * writes to the file specified by DFileID from the buffer starting from the
-	 * buffer offset startOffsetl at most count bytes are transferred
-	 */
-	public int write(int dFID, byte[] buffer, int startOffset, int count) {
-		if (myFreeBlocks.size() == 0) {
-			System.out.println("VOLUME SIZE EXCEEDED");
-			return -1;
-		}
-		DFileID file = new DFileID(dFID);
-		ArrayList<Integer> iNodeInfo = parseINode(file);
-		int bytesWritten = 0;
-		int vBlockNumber = 1;
-		while (bytesWritten < count
-				&& buffer.length > (startOffset + bytesWritten)) {
-			DBuffer nextBlock;
-			if (vBlockNumber < iNodeInfo.get(0)) {
-				nextBlock = myDBCache.getBlock(iNodeInfo.get(vBlockNumber));
-				vBlockNumber++;
-			} else {
-				DBuffer iNodeBlock = myDBCache.getBlock(file.block());
-				byte[] blockBuffer = new byte[Constants.BLOCK_SIZE];
-				iNodeBlock.read(buffer, 0, Constants.BLOCK_SIZE);
-				ByteBuffer b = ByteBuffer.allocate(Constants.INODE_SIZE);
-				int nextPBlockNumber;
-				synchronized (this) {
-					nextPBlockNumber = myFreeBlocks.first();
-					myFreeBlocks.remove(0);
-				}
-				iNodeInfo.set(0, iNodeInfo.get(0) + 1);
-				iNodeInfo.add(nextPBlockNumber);
-				try {
-					for (int i : iNodeInfo)
-						b.putInt(i);
-
-				} catch (BufferOverflowException e) {
-					System.out.println("MAX FILE SIZE EXCEEDED");
-					return -1;
-				}
-				for (int j = 0; j < Constants.INODE_SIZE; j++) {
-					blockBuffer[j + file.offset()] = b.array()[j];
-				}
-				iNodeBlock.write(blockBuffer, 0, Constants.BLOCK_SIZE);
-				iNodeBlock.release();
-				nextBlock = myDBCache.getBlock(nextPBlockNumber);
-				vBlockNumber++;
-			}
-			bytesWritten += nextBlock.write(buffer, startOffset + bytesWritten,
-					count - bytesWritten);
-			nextBlock.release();
-		}
-		return count;
-	}
-
-	/* returns the size in bytes of the file indicated by DFileID. */
-	public int sizeDFile(int dFID) {
-		ArrayList<Integer> iNodeInfo = parseINode(new DFileID(dFID));
-		return iNodeInfo.get(0)*Constants.BLOCK_SIZE;
-	}
-
-	/*
-	 * List all the existing DFileIDs in the volume
-	 */
-	public void sync() {
-		myDBCache.sync();
-	}
-
-    private synchronized boolean acquireReadLock(int dFID)
+    private synchronized boolean acquireReadLock(DFileID dFID)
     {
         LockState state = myDFiles.get(dFID);
         if (state == null) return false;
@@ -262,7 +262,7 @@ public class DFS {
         return true;
     }
 
-    private synchronized boolean releaseReadLock(int dFID)
+    private synchronized boolean releaseReadLock(DFileID dFID)
     {
         LockState state = myDFiles.get(dFID);
         if (state == null) return false;
@@ -270,7 +270,7 @@ public class DFS {
         return true;
     }
 
-    private synchronized boolean acquireWriteLock(int dFID)
+    private synchronized boolean acquireWriteLock(DFileID dFID)
     {
         LockState state = myDFiles.get(dFID);
         if (state == null) return false;
@@ -291,7 +291,7 @@ public class DFS {
         return true;
     }
 
-    private synchronized boolean releaseWriteLock(int dFID)
+    private synchronized boolean releaseWriteLock(DFileID dFID)
     {
         LockState state = myDFiles.get(dFID);
         if (state == null) return false;
