627c5791774cc00ab0488f0afe297d85c7e03a4e
benberg <ben.berg@duke.edu>
2012-12-01 21:40:55 -0500

    merged.

--- src/test/PersistenceTests.java
+++ src/test/PersistenceTests.java
@@ -14,21 +14,27 @@ import org.junit.Test;
 
 import common.Constants;
 
+
+import java.nio.ByteBuffer;
+
+import org.junit.Before;
+import org.junit.Test;
+import common.Constants;
+
+import common.DFileID;
+
+
 import dfs.DFS;
 
 
 public class PersistenceTests {
+	private DFS dfs;
 
 	@Before
 	public void setUp() throws IOException {
 		copyFile("test_disk.dat", "DSTORE.dat");
 		DFS dfs = new DFS(false);
 		dfs.init();
-	}
-
-	@Test
-	public void test() {
-
 	}
 
 	public void copyFile(String srcLocation, String dstLocation) throws IOException {
@@ -48,4 +54,25 @@ public class PersistenceTests {
         inStream.close();
         outStream.close();
 	}
+	
+	@Test
+	public void persistingOfWrites() {
+		DFileID originalFile = dfs.createDFile();
+		ByteBuffer b=ByteBuffer.allocate(4);
+		b.putInt(333);
+		dfs.write(originalFile, b.array(), 0, 4);
+		for(int i=0; i<30; i++){
+			DFileID newFile= dfs.createDFile();
+			ByteBuffer contents=ByteBuffer.allocate(50*Constants.BLOCK_SIZE);
+			for(int j=0; j<50*Constants.BLOCK_SIZE/4; j++){
+				contents.putInt(1);
+			}
+			dfs.write(newFile, contents.array(), 0, 50*Constants.BLOCK_SIZE);
+		}
+		byte[] result=new byte[4];
+		dfs.read(originalFile, result, 0, 4);
+		assert(333==ByteBuffer.wrap(result).getInt());
+		
+	}
+
 }
